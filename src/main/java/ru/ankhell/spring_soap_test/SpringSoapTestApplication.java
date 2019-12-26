ca29cd20287229260293cbd182c695be95c16de6
Dmitry D. (ankhell) <ankhell.java@gmail.com>
2019-12-26 16:17:52 +0300

    First commit

---
+++ src/main/java/ru/ankhell/spring_soap_test/SpringSoapTestApplication.java
@@ -0,0 +1,13 @@
+package ru.ankhell.spring_soap_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ankhell/spring_soap_test/SpringSoapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ankhell.spring_soap_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
